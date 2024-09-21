9529ce0b8b0acffc18e72fa4183b1937dcf48156
Rolandas Macius <rolandas.macius@gmail.com>
2024-09-21 21:58:38 +0300

    edit todo controller3

--- services/conversationsService/middleware/authMiddleware.js
+++ services/conversationsService/middleware/authMiddleware.js
@@ -3,7 +3,7 @@ const { sendCoteMessageToGetClientData } = require("../plugin");
 module.exports = {
   authMiddleware: (req, res, next) => {
     const token = req.cookies.authtoken;
-    // console.log(token + "123");
+    console.log(token + "123");
     sendCoteMessageToGetClientData(token).then((response) => {
       // console.log("response123", response);
       if (!response) {
