76dacc68dd4cdee35d6ce09d46fe3e9ebbcb65b1
Rolandas Macius <rolandas.macius@gmail.com>
2024-09-21 20:52:19 +0300

    edit todo controller1

--- services/todoService/routes/todoRoutes.js
+++ services/todoService/routes/todoRoutes.js
@@ -4,7 +4,7 @@ const todoController = require("../controllers/todoController");
 const { authMiddleware1 } = require("../middleware/authMiddleware1");
 const router = express.Router();
 
-router.get("/", todoController.getTodos);
+router.get("/", authMiddleware1, todoController.getTodos);
 router.post("/", authMiddleware1, todoController.createTodo);
 router.patch("/", authMiddleware1, todoController.markAsCompleted);
 router.delete("/", authMiddleware1, todoController.deleteTodo);
