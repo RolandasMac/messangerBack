b6e918593b478449bd33cd2d5178d2f8bcee6522
Rolandas Macius <rolandas.macius@gmail.com>
2024-09-21 22:39:22 +0300

    edit todo controller4

--- services/conversationsService/controllers/conversationsController.js
+++ services/conversationsService/controllers/conversationsController.js
@@ -318,8 +318,8 @@ exports.notifyClientBySocket = async (req, res) => {
 
 exports.getConversationsList = async (req, res) => {
   const { id } = req.tokenInfo;
-  console.log("id");
-  console.log(id);
+  // console.log("id");
+  // console.log(id);
   // const userId = "66cdb7e7aff7b6e274913c98";
   // return res.status(200).json("Gaidys");
   try {
